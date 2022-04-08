385551c7ac5b70312a6bfbe37035ce4c2debc356
Yasikhorram <khorramyasaman@gmail.com>
2022-04-08 10:14:39 -0500

    skeleton of salesPath func

---
+++ salesPath.js
@@ -0,0 +1,16 @@
+/*
+The root is the company itself
+every node in the tree represents a car distributor
+The leaf nodes are car dealerships
+every node holds an integer that is the cost of shipping a car to it
+*/
+function getCheapestCost(rootNode) {
+  salesPathMin = 0;
+
+  return salesPathMin;
+}
+
+function Node(cost) {
+  this.cost = cost;
+  this.children = [];
+}
